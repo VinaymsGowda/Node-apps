d2ee2bfa49a8426ad76fa1220f5f62dc72f11eed
Vinay MS <vinaymsgowda27@gmail.com>
2024-03-04 11:40:43 +0530

    first commit

---
+++ container2/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { startMetricsServer } from "./metrics.js";
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    console.log("Hello World");
+    res.send("Hit / Route ");
+})
+app.get("/route1",(req,res)=>{
+    console.log("Hello World");
+    res.send("Hit /Route1 route ");
+})
+app.get("/route2",(req,res)=>{
+    console.log("Hello World");
+    res.send("Hit /Route2 ");
+})
+
+app.listen(7000,()=>{
+    console.log("Port running on Port 4500");
+})
+startMetricsServer();
---
+++ container2/metrics.js
@@ -0,0 +1,18 @@
+import express from "express";
+import client from "prom-client";
+
+const app = express();
+
+export function startMetricsServer() {
+    const collectDefaultMetrics = client.collectDefaultMetrics;
+    collectDefaultMetrics();
+
+    app.get("/metrics", async (req, res) => {
+        res.set("Content-Type", client.register.contentType);
+        return res.send(await client.register.metrics());
+    });
+
+    app.listen(6001, () => {
+        console.log("Metrics Server Started at port 6001");
+    });
+}
